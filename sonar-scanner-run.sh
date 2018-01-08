d9b046330541975b7bbc2c4ae1022302b75e2d62
Òscar Casajuana <elboletaire@underave.net>
2018-01-08 19:02:22 +0100

    Properly check for (un)defined bash variables
    See
    https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
    for more details

--- sonar-scanner-run.sh
+++ sonar-scanner-run.sh
@@ -20,51 +20,51 @@ if [ -z ${SONAR_GITLAB_PROJECT_ID+x} ]; then
   SONAR_GITLAB_PROJECT_ID=$CI_PROJECT_ID
 fi
 
-if [ ! -z "$SONAR_PROJECT_KEY" ]; then
+if [ ! -z ${SONAR_PROJECT_KEY+x} ]; then
   COMMAND="$COMMAND -Dsonar.projectKey=$SONAR_PROJECT_KEY"
 fi
 
-if [ ! -z "$SONAR_TOKEN" ]; then
+if [ ! -z ${SONAR_TOKEN+x} ]; then
   COMMAND="$COMMAND -Dsonar.login=$SONAR_TOKEN"
 fi
 
-if [ ! -z "$SONAR_PROJECT_VERSION" ]; then
+if [ ! -z ${SONAR_PROJECT_VERSION+x} ]; then
   COMMAND="$COMMAND -Dsonar.projectVersion=$SONAR_PROJECT_VERSION"
 fi
 
-if [ ! -z "$SONAR_DEBUG" ]; then
+if [ ! -z ${SONAR_DEBUG+x} ]; then
   COMMAND="$COMMAND -X"
 fi
 
-if [ ! -z "$SONAR_SOURCES" ]; then
+if [ ! -z ${SONAR_SOURCES+x} ]; then
   COMMAND="$COMMAND -Dsonar.sources=$SONAR_SOURCES"
 fi
 
-if [ ! -z "$SONAR_PROFILE" ]; then
+if [ ! -z ${SONAR_PROFILE+x} ]; then
   COMMAND="$COMMAND -Dsonar.profile=$SONAR_PROFILE"
 fi
 
-if [ ! -z "$SONAR_GITLAB_PROJECT_ID" ]; then
+if [ ! -z ${SONAR_GITLAB_PROJECT_ID+x} ]; then
   COMMAND="$COMMAND -Dsonar.gitlab.project_id=$SONAR_GITLAB_PROJECT_ID"
 fi
 
-if [ ! -z "$SONAR_LANGUAGE" ]; then
+if [ ! -z ${SONAR_LANGUAGE+x} ]; then
   COMMAND="$COMMAND -Dsonar.language=$SONAR_LANGUAGE"
 fi
 
-if [ ! -z "$SONAR_ENCODING" ]; then
+if [ ! -z ${SONAR_ENCODING+x} ]; then
   COMMAND="$COMMAND -Dsonar.sourceEncoding=$SONAR_ENCODING"
 fi
 
-if [ ! -z $CI_BUILD_REF ]; then
+if [ ! -z ${CI_BUILD_REF+x} ]; then
   COMMAND="$COMMAND -Dsonar.gitlab.commit_sha=$CI_BUILD_REF"
 fi
 
-if [ ! -z $CI_BUILD_REF_NAME ]; then
+if [ ! -z ${CI_BUILD_REF_NAME+x} ]; then
   COMMAND="$COMMAND -Dsonar.gitlab.ref_name=$CI_BUILD_REF_NAME"
 fi
 
-if [ ! -z $SONAR_BRANCH ]; then
+if [ ! -z ${SONAR_BRANCH+x} ]; then
   COMMAND="$COMMAND -Dsonar.branch=$SONAR_BRANCH"
 fi
 
@@ -73,7 +73,7 @@ if [ -z ${SONAR_ANALYSIS_MODE+x} ]; then
 fi
 
 # `analysis by default
-if [ ! -z $SONAR_ANALYSIS_MODE ]; then
+if [ ! -z ${SONAR_ANALYSIS_MODE+x} ]; then
   COMMAND="$COMMAND -Dsonar.analysis.mode=$SONAR_ANALYSIS_MODE"
   if [ $SONAR_ANALYSIS_MODE="preview" ]; then
     COMMAND="$COMMAND -Dsonar.issuesReport.console.enable=true"
